0269b19576fed2e81c1875a27309c660cc6d48a5
Steve <slshotts77@gmail.com>
2021-03-30 20:19:23 -0400

    Add view to index

---
+++ ElevenNote.WebMVC/Controllers/NoteController.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElevenNote.WebMVC.Controllers
+{
+    public class NoteController : Controller
+    {
+        // GET: Note
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
