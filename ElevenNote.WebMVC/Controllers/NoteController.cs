4305e3add1ce5dc5c449dd260167a832e8c6386d
Steve <slshotts77@gmail.com>
2021-04-01 17:41:23 -0400

    Added GetNotes method

--- ElevenNote.WebMVC/Controllers/NoteController.cs
+++ ElevenNote.WebMVC/Controllers/NoteController.cs
@@ -1,4 +1,5 @@
 ﻿
+using ElevenNote.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,12 +8,32 @@ using System.Web.Mvc;
 
 namespace ElevenNote.WebMVC.Controllers
 {
+    [Authorize]
     public class NoteController : Controller
     {
         // GET: Note
         public ActionResult Index()
         {
+            var model = new NoteListItem[0];
+
+            return View(model);
+        }
+
+        //GET
+        public ActionResult Create()
+        {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(NoteCreate model)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            return View(model);
         }
     }
 }
